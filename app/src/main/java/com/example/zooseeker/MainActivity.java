f021d8d5d99de095fed6bf09191ceece447a080f
Shane Kim <shanekim28@gmail.com>
2022-04-15 11:49:54 -0700

    Inject viewmodel
    Injects a viewmodel to prepare for MVVM architecture

--- app/src/main/java/com/example/zooseeker/MainActivity.java
+++ app/src/main/java/com/example/zooseeker/MainActivity.java
@@ -2,25 +2,25 @@ package com.example.zooseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import com.example.zooseeker.databinding.ActivityMainBinding;
+import com.example.zooseeker.viewmodels.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Set view binding
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        // Inject viewmodel instance
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
-        /* replaces this:
-         *      TextView tv = findViewById(R.id.mytextview);
-         *      tv.setText("hello world!");
-         *  with this:
-         *      binding.mytextview.setText("view binding is awesome!");
-         */
+        // TODO: Observe changes of LiveData objects
     }
 }
