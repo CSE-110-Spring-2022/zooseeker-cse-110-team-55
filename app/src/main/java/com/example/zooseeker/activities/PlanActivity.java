52378fbf93497cf0646d445b07cd88843a9f3039
Andy Lin <76961516+AndyLin910121@users.noreply.github.com>
2022-05-03 18:07:37 -0700

    route

--- app/src/main/java/com/example/zooseeker/activities/PlanActivity.java
+++ app/src/main/java/com/example/zooseeker/activities/PlanActivity.java
@@ -1,19 +1,27 @@
 package com.example.zooseeker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.zooseeker.R;
+import com.example.zooseeker.viewmodels.PlanViewModel;
 
 public class PlanActivity extends AppCompatActivity {
+    private PlanViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+        Intent intent = getIntent();
+        viewModel = new ViewModelProvider(this).get(PlanViewModel.class);
+        viewModel.setPlan(viewModel.getRoute(intent.getStringArrayListExtra("selected_animals")));
     }
+
 
     public void onBackClicked(View view) {
         finish();
