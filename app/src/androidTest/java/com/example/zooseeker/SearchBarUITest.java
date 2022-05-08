587e30a34f60aa5d993c45d6dca3b07f9e095f55
alfredxus <alfredxus@gmail.com>
2022-05-08 01:32:50 -0700

    Fixed expresso test

--- app/src/androidTest/java/com/example/zooseeker/SearchBarUITest.java
+++ app/src/androidTest/java/com/example/zooseeker/SearchBarUITest.java
@@ -25,7 +25,6 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
-import com.example.zooseeker.R;
 import com.example.zooseeker.activities.HomeActivity;
 
 import org.hamcrest.Description;
@@ -44,57 +43,57 @@ public class SearchBarUITest {
     public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
 
     @Test
-    public void searchBarUITest() {
-        ViewInteraction searchAutoComplete = onView(
-                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")),
-                        childAtPosition(
-                                allOf(withClassName(is("android.widget.LinearLayout")),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                1)),
-                                0),
-                        isDisplayed()));
-        searchAutoComplete.perform(replaceText("lion"), closeSoftKeyboard());
-
-        ViewInteraction searchAutoComplete2 = onView(
-                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("lion"),
-                        childAtPosition(
-                                allOf(withClassName(is("android.widget.LinearLayout")),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                1)),
-                                0),
-                        isDisplayed()));
-        searchAutoComplete2.perform(pressImeActionButton());
-
+    public void searchBarUITest2() {
         ViewInteraction textView = onView(
-                allOf(withId(R.id.animal_name), withText("Lions"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
-                        isDisplayed()));
-        textView.check(matches(withText("Lions")));
-
+allOf(withId(R.id.count), withText("0"),
+withParent(allOf(withId(R.id.topbar),
+withParent(IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class)))),
+isDisplayed()));
+        textView.check(matches(withText("0")));
+        
+        ViewInteraction searchAutoComplete = onView(
+allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")),
+childAtPosition(
+allOf(withClassName(is("android.widget.LinearLayout")),
+childAtPosition(
+withClassName(is("android.widget.LinearLayout")),
+1)),
+0),
+isDisplayed()));
+        searchAutoComplete.perform(replaceText("lion"), closeSoftKeyboard());
+        
+        ViewInteraction searchAutoComplete2 = onView(
+allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")), withText("lion"),
+childAtPosition(
+allOf(withClassName(is("android.widget.LinearLayout")),
+childAtPosition(
+withClassName(is("android.widget.LinearLayout")),
+1)),
+0),
+isDisplayed()));
+        searchAutoComplete2.perform(pressImeActionButton());
+        
         ViewInteraction textView2 = onView(
-                allOf(withId(R.id.count), withText("0"),
-                        withParent(allOf(withId(R.id.topbar),
-                                withParent(IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class)))),
-                        isDisplayed()));
-        textView2.check(matches(withText("0")));
-
+allOf(withId(R.id.animal_name), withText("Lions"),
+withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class))),
+isDisplayed()));
+        textView2.check(matches(withText("Lions")));
+        
         ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.recyclerView),
-                        childAtPosition(
-                                withClassName(is("android.widget.RelativeLayout")),
-                                1)));
+allOf(withId(R.id.recyclerView),
+childAtPosition(
+withClassName(is("android.widget.RelativeLayout")),
+1)));
         recyclerView.perform(actionOnItemAtPosition(0, click()));
-
+        
         ViewInteraction textView3 = onView(
-                allOf(withId(R.id.count), withText("1"),
-                        withParent(allOf(withId(R.id.topbar),
-                                withParent(IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class)))),
-                        isDisplayed()));
+allOf(withId(R.id.count), withText("1"),
+withParent(allOf(withId(R.id.topbar),
+withParent(IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class)))),
+isDisplayed()));
         textView3.check(matches(withText("1")));
-    }
-
+        }
+    
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
@@ -109,8 +108,8 @@ public class SearchBarUITest {
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
+                        && view.equals(((ViewGroup)parent).getChildAt(position));
             }
         };
     }
-}
+    }
